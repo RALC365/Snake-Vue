402524c7a1748b15147fa07e1cb8d307b3696a47
RALC365 <alejandro.carcamo@unitec.edu>
2020-07-14 19:11:01 -0600

    Juego funcional y listo

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -16,6 +16,206 @@
 	</div>
 </template>
 
+<script>
+	import Grid from '../app/Grid.js'
+	const UP = [-1, 0]
+	const DOWN = [1, 0]
+	const LEFT = [0, -1]
+	const RIGHT = [0, 1]
+	let keyMap = {
+		"37": LEFT,
+		"38": UP,
+		"39": RIGHT,
+		"40": DOWN
+	}
+	let feeding = []
+	let length = 3
+	let oldFood = []
+	let snakePos = []
+	let snakeCells = []
+	let ticking = []
+	let userActions = []
+	export default {
+		/**
+		 * The component's local methods.
+		 *
+		 * @type {Object}
+		 */
+		methods: {
+			/**
+			 * Determine if the snake should eat.
+			 *
+			 * @return {void}
+			 */
+			eatFood() {
+				if (snakePos && snakePos.food) {
+					this.score++
+					length++
+					snakeCells.forEach(cell => cell.snake++)
+					snakePos.food = false
+					this.setFood()
+				}
+			},
+			/**
+			 * Finish the game.
+			 *
+			 * @return {void}
+			 */
+			gameOver() {
+				this.dead = true
+				clearInterval(ticking)
+				clearInterval(feeding)
+			},
+			/**
+			 * Get a random number between the given interval.
+			 *
+			 * @param {Number} min
+			 * @param {Number} max
+			 * @return {Number}
+			 */
+			getRandomNumber(min, max) {
+				return Math.floor(Math.random() * (max - min + 1) + min)
+			},
+			/**
+			 * Handle the user action event.
+			 *
+			 * @param {String} key
+			 * @return {void}
+			 */
+			handleUserAction(key) {
+				let direction = keyMap[key];
+				if (!direction) {
+					return
+				}
+				userActions.push(() => {
+					if (!this.isOppositeDirection(direction, this.direction)) {
+						this.direction = direction
+					}
+				});
+			},
+			/**
+			 * Determine if the new direction is the opposite to the current one.
+			 *
+			 * @param {Array} newDirection
+			 * @return {Boolean}
+			 */
+			isOppositeDirection(newDirection) {
+				return newDirection[0] + this.direction[0] === 0 && newDirection[1] + this.direction[1] === 0
+			},
+			/**
+			 * Move the snake to the next position.
+			 *
+			 * @return {void}
+			 */
+			moveSnake() {
+				let { x, y } = snakePos
+				let [xd, yd] = this.direction
+				snakePos = (this.grid[x + xd] || [])[y + yd]
+				if (!snakePos) {
+					if (this.size > 15) {
+						this.size = this.size - (this.size * 0.166)
+						clearInterval(ticking)
+						clearInterval(feeding)
+						return this.start(this.size)
+					}
+					return this.gameOver()
+				}
+				if (snakePos.snake || !snakePos) {
+					return this.gameOver();
+				}
+				snakeCells.forEach(cell => cell.snake--);
+				snakeCells = snakeCells.filter(cell => cell.snake > 0);
+				snakePos.snake = length;
+				snakeCells.push(snakePos);
+			},
+			/**
+			 * Add a piece of food into the grid.
+			 *
+			 * @return {void}
+			 */
+			setFood() {
+				oldFood.food = false;
+				oldFood = Grid.random(this.grid)
+				oldFood.food = true;
+			},
+			/**
+			 * Start the game.
+			 *
+			 * @param {Number} size
+			 * @return {void}
+			 */
+			start(size = 60) {
+				this.size = size
+				let snakeSpeed = 50
+				let foodSpeed = this.getRandomNumber(4000, 10000)
+				this.grid = new Grid(size, (x, y) => ({
+					x,
+					y,
+					snake: 0,
+					food: false
+				}))
+				this.dead = false;
+				userActions = [];
+				ticking = setInterval(this.tick, snakeSpeed);
+				snakePos = Grid.random(this.grid);
+				snakePos.snake = length;
+				snakeCells = [snakePos];
+				feeding = setInterval(this.setFood, foodSpeed);
+			},
+			/**
+			 * Reset all the data.
+			 *
+			 * @return {void}
+			 */
+			reset() {
+				location.reload()
+			},
+			/**
+			 * Determine the snake have to do in every tick.
+			 *
+			 * @return {void}
+			 */
+			tick() {
+				let userAction = userActions.shift()
+				if (userAction) {
+					userAction()
+				}
+				this.moveSnake()
+				this.eatFood()
+			},
+		},
+		/**
+		 * The component's before create lifecycle hook.
+		 *
+		 * @return {void}
+		 */
+		created() {
+			this.start();
+			window.addEventListener('keydown', event => this.handleUserAction(event.which))
+		},
+		/**
+		 * Get the component's initial state.
+		 *
+		 * @return {Object}
+		 */
+		data() {
+			return {
+				dead: false,
+				direction: UP,
+				grid: null,
+				score: 0,
+				size: 60,
+			}
+		},
+	/**
+	 * The component's name used for debugging.
+	 *
+	 * @type {String}
+	 */
+	name: 'Game',
+	}
+</script>
+
 <style>
  .container {
 display: flex;
